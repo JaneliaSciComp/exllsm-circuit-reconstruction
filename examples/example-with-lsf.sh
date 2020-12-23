abcd1b4ff9cbbead198897a7c4bc9b841670fb00
Cristian Goina <goinac@janelia.hhmi.org>
2020-12-23 16:16:57 -0500

    fixed the example script

--- examples/example-with-lsf.sh
+++ examples/example-with-lsf.sh
@@ -1,10 +1,10 @@
-./main.nf -profile lsf \
+./main.nf \
+        -profile lsf \
         --runtime_opts "-e -B /nrs/scicompsoft/goinac" \
         --lsf_opts "-P scicompsoft" \
         --workers 3 \
         --worker_cores 3 \
         --driver_memory 10g \
         --spark_work_dir "$PWD/local" \
-        --stitching_app "external-modules/stitching-spark/target/stitching-spark-1.8.2-SNAPSHOT.jar"
+        --stitching_app "external-modules/stitching-spark/target/stitching-spark-1.8.2-SNAPSHOT.jar" \
         --data_dir /nrs/scicompsoft/goinac/lillvis/DA1/images
-
