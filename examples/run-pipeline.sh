ad541a314517d3a308be46a6f5b96bdd63d89602
Cristian Goina <goinac@janelia.hhmi.org>
2021-03-10 12:53:33 -0500

    update example

---
+++ examples/run-pipeline.sh
@@ -0,0 +1,17 @@
+PROFILE=$1
+PROJECT_CODE=$2
+
+./main.nf \
+        -profile $PROFILE \
+        --runtime_opts "-e -B /nrs/scicompsoft/goinac -B /groups/dickson/dicksonlab/lillvis" \
+        --lsf_opts "-P $PROJECT_CODE" \
+        --workers 1 \
+        --worker_cores 6 \
+        --driver_memory 10g \
+        --spark_work_dir "$PWD/local" \
+        --stitching_app "$PWD/external-modules/stitching-spark/target/stitching-spark-1.8.2-SNAPSHOT.jar" \
+        --datasets DA1 \
+        --data_dir /nrs/scicompsoft/goinac/lillvis \
+        --output_dir /nrs/scicompsoft/goinac/lillvis/results \
+        --psf_dir /groups/dickson/dicksonlab/lillvis/ExM/lattice/PSFs/20200928/PSFs \
+        --deconv_cores 4
