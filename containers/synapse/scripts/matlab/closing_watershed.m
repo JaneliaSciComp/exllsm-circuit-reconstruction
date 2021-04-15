7ee6576c274d1777eee4f9d63980cf8b6f535d31
Cristian Goina <goinac@hhmi.org>
2021-04-15 16:19:36 -0400

    fixed container

--- containers/synapse/scripts/matlab/closing_watershed.m
+++ containers/synapse/scripts/matlab/closing_watershed.m
@@ -1,6 +1,6 @@
 function flag = closing_watershed(img_name)
     [data_path, name, ext] = fileparts(img_name);
-    disp(['Loading for watershed segmentation', img_file]);
+    disp(['Loading for watershed segmentation', img_name]);
     img = read_tif(img_name);
     disp('Close and watershed transform...', img_name);
     img = close_and_watershed_transform(img);
