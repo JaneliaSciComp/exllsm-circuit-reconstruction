d064efb99244f635db3b3dc9be1db453112597e6
Cristian Goina <goinac@hhmi.org>
2021-04-16 12:32:36 -0400

    formatting

--- containers/synapse/scripts/matlab/close_and_watershed_transform.m
+++ containers/synapse/scripts/matlab/close_and_watershed_transform.m
@@ -1,17 +1,16 @@
 function seg_img = close_and_watershed_transform(img)
     disp('Morphological closing...')
     closed_img = closing_img(img);
-    disp('Watershed segmentation...');
+    disp('Starrt watershed segmentation...');
     seg_img = water_shed(closed_img);
+    disp('Completed watershed segmentation...');
 end
 
 function seg_img = closing_img(img)
     % Image closing
-    img(img~=0)=1;
-    SE=strel('sphere',3);
-    seg_img=imclose(img,SE);
-    % image filling
-    % seg_img=imfill(seg_img,'holes');
+    img(img~=0) = 1;
+    SE = strel('sphere',3);
+    seg_img = imclose(img,SE);
     seg_img(seg_img~=0) = 255;
 end
 
