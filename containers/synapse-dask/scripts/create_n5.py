f89d1ed758ef8e571aff421a20750765aea90d60
Konrad Rokicki <rokickik@janelia.hhmi.org>
2021-05-05 23:30:45 -0400

    support compression option

--- containers/synapse-dask/scripts/create_n5.py
+++ containers/synapse-dask/scripts/create_n5.py
@@ -2,24 +2,39 @@
 
 import argparse
 import zarr
+import numcodecs as codecs
 
-def create_dataset(output_n5, template_n5, dtype_override):
+def create_dataset(output_n5, template_n5, compression='same', dtype='same', overwrite=True):
 
-    subpath = '/s0'
-    template = zarr.open(store=zarr.N5Store(template_n5), mode='r')[subpath]
+    data_set = '/s0'
+    template = zarr.open(store=zarr.N5Store(template_n5), mode='r')[data_set]
+    out = zarr.open(store=zarr.N5Store(output_n5), mode='a')
+        
+    if compression == 'raw':
+        compressor = None
+    elif compression == 'same':
+        compressor = template.compressor
+    else:
+        compressor = codecs.get_codec(dict(id=compression))
+
+    if dtype=='same':
+        dtype = template.dtype
+        
+    print("Using compressor:", compressor or 'raw')
 
     print("Creating n5 data set with:")
-    print("  shape", template.shape)
-    print("  chunks", template.chunks)
+    print(f"  compressor: {compressor}")
+    print(f"  shape:      {template.shape}")
+    print(f"  chunking:   {template.chunks}")
+    print(f"  dtype:      {dtype}")
+    print(f"  to path:    {output_n5}{data_set}")
 
-    out = zarr.open(store=zarr.N5Store(output_n5), mode='a')
-
-    if dtype_override:
-        dtype = dtype_override
-    else:
-        dtype = template.dtype
-
-    out.create_dataset(subpath, shape=template.shape, chunks=template.chunks, dtype=dtype, overwrite=True)
+    out.create_dataset(data_set,
+        shape=template.shape,
+        chunks=template.chunks,
+        dtype=dtype,
+        compressor=compressor,
+        overwrite=overwrite)
 
 
 def main():
@@ -32,12 +47,16 @@ def main():
     parser.add_argument('-t', '--template', dest='template_path', type=str, required=True, \
         help='Path to an existing n5 to use as a template')
 
-    parser.add_argument('--dtype', dest='dtype', type=str, default=None, \
-        help='Set the output dtype')
+    parser.add_argument('--dtype', dest='dtype', type=str, default='same', \
+        help='Set the output dtype. Default is the same dtype as the template.')
+
+    parser.add_argument('--compression', dest='compression', type=str, default='same', \
+        help='Set the compression. Valid values any codec id supported by numcodecs including: '+ \
+             'raw, lz4, gzip, bz2, blosc. Default is the same compression as the template.')
 
     args = parser.parse_args()
     
-    create_dataset(args.output_path, args.template_path, args.dtype)
+    create_dataset(args.output_path, args.template_path, args.compression, args.dtype)
 
 
 if __name__ == "__main__":
