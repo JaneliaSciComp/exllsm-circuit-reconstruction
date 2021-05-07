4994a4adf41ce1e333408a397743e453bde25bbe
Cristian Goina <goinac@hhmi.org>
2021-05-07 13:30:12 -0400

    checks for the case when there are no input csvs; update container version

--- containers/scripts/create-containers.sh
+++ containers/scripts/create-containers.sh
@@ -1,5 +1,5 @@
 docker build \
-    -t registry.int.janelia.org/exm-analysis/synapse:1.1.0 \
-    -t synapse:1.1.0 \
+    -t registry.int.janelia.org/exm-analysis/synapse:1.2.0 \
+    -t synapse:1.2.0 \
     -t synapse \
     containers/synapse
