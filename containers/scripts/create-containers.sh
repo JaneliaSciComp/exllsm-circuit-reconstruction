31123059fa0ec166c52ce1f3bc08335fad5c4a81
Cristian Goina <goinac@hhmi.org>
2022-06-29 13:56:17 -0400

    changed the scripts to be able to echo commands

--- containers/scripts/create-containers.sh
+++ containers/scripts/create-containers.sh
@@ -2,36 +2,43 @@ DIR=$(cd "$(dirname "$0")"; pwd)
 
 source ${DIR}/container-versions.sh
 
-docker build \
+RUNNER=echo
+
+${RUNNER} docker build \
     --build-arg SYNAPSE_DETECTOR_GIT_TAG=${synapse_git_hash} \
     -t registry.int.janelia.org/exm-analysis/synapse:${synapse_version} \
+    -t public.ecr.aws/janeliascicomp/exm-analysis/synapse:${synapse_version} \
     -t synapse:${synapse_version} \
     -t synapse \
     containers/synapse
 
-docker build \
+${RUNNER} docker build \
     -t registry.int.janelia.org/exm-analysis/synapse-dask:${synapse_dask_version} \
+    -t public.ecr.aws/janeliascicomp/exm-analysis/synapse-dask:${synapse_dask_version} \
     -t synapse-dask:${synapse_dask_version} \
     -t synapse-dask \
     containers/synapse-dask
 
-docker build \
+${RUNNER} docker build \
     --build-arg NEURON_SEGMENTATION_GIT_TAG=${neuron_segmentation_git_hash} \
     -t registry.int.janelia.org/exm-analysis/neuron-segmentation:${neuron_segmentation_version} \
+    -t public.ecr.aws/janeliascicomp/exm-analysis/neuron-segmentation:${neuron_segmentation_version} \
     -t neuron-segmentation:${neuron_segmentation_version} \
     -t neuron-segmentation \
     containers/neuron-segmentation
 
-docker build \
+${RUNNER} docker build \
     --build-arg STITCHING_SPARK_GIT_TAG=${stitching_git_hash} \
     -t registry.int.janelia.org/exm-analysis/stitching:${stitching_version} \
+    -t public.ecr.aws/janeliascicomp/exm-analysis/stitching:${stitching_version} \
     -t stitching:${stitching_version} \
     -t stitching \
     containers/stitching
 
-docker build \
+${RUNNER} docker build \
     --build-arg STITCHING_SPARK_GIT_TAG=${n5_spark_tools_git_hash} \
     -t registry.int.janelia.org/exm-analysis/n5-spark-tools:${n5_spark_tools_version} \
+    -t public.ecr.aws/janeliascicomp/exm-analysis/n5-spark-tools:${n5_spark_tools_version} \
     -t n5-spark-tools:${n5_spark_tools_version} \
     -t n5-spark-tools \
     containers/n5-spark-tools
--- containers/scripts/push-containers.sh
+++ containers/scripts/push-containers.sh
@@ -2,8 +2,18 @@ DIR=$(cd "$(dirname "$0")"; pwd)
 
 source ${DIR}/container-versions.sh
 
-docker push registry.int.janelia.org/exm-analysis/synapse:${synapse_version}
-docker push registry.int.janelia.org/exm-analysis/synapse-dask:${synapse_dask_version}
-docker push registry.int.janelia.org/exm-analysis/neuron-segmentation:${neuron_segmentation_version}
-docker push registry.int.janelia.org/exm-analysis/stitching:${stitching_version}
-docker push registry.int.janelia.org/exm-analysis/n5-spark-tools:${n5_spark_tools_version}
+REPO=$1
+
+if [[ -z ${REPO} ]] ; then
+    REPO=registry.int.janelia.org/exm-analysis
+elif [[ "${REPO}" == "aws" ]] ; then
+    REPO=public.ecr.aws/janeliascicomp/exm-analysis
+fi
+
+RUNNER=echo
+
+${RUNNER} docker push ${REPO}/synapse:${synapse_version}
+${RUNNER} docker push ${REPO}/synapse-dask:${synapse_dask_version}
+${RUNNER} docker push ${REPO}/neuron-segmentation:${neuron_segmentation_version}
+${RUNNER} docker push ${REPO}/stitching:${stitching_version}
+${RUNNER} docker push ${REPO}/n5-spark-tools:${n5_spark_tools_version}
