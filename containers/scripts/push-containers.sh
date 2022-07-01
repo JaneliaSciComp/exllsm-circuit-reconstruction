0546661fb45299ecd83999641778f9ae7434eaf8
Cristian Goina <goinac@hhmi.org>
2022-07-01 12:20:02 -0400

    changed push containers script to use flag input args

--- containers/scripts/push-containers.sh
+++ containers/scripts/push-containers.sh
@@ -2,15 +2,41 @@ DIR=$(cd "$(dirname "$0")"; pwd)
 
 source ${DIR}/container-versions.sh
 
-REPO=$1
+RUNNER=
+REPO=
+
+help_cmd="$0 [-n] [-r <repo>] [-h|--help]"
+
+while [[ $# > 0 ]]; do
+    key="$1"
+    shift # past the key
+    case $key in
+        -n)
+            RUNNER="echo"
+            ;;
+        -r)
+            REPO="$1"
+	    shift
+            ;;
+        -h|--help)
+            echo "${help_cmd}"
+            exit 0
+            ;;
+        *)
+            echo "Unknown flag ${key}"
+            echo "${help_cmd}"
+            exit 1
+            ;;
+    esac
+done
 
 if [[ -z ${REPO} ]] ; then
+    REPO=registry.int.janelia.org/exm-analysis
+elif [[ "${REPO}" == "janelia" ]] ; then
     REPO=registry.int.janelia.org/exm-analysis
 elif [[ "${REPO}" == "aws" ]] ; then
     REPO=public.ecr.aws/janeliascicomp/exm-analysis
 fi
-
-RUNNER=echo
 
 ${RUNNER} docker push ${REPO}/synapse:${synapse_version}
 ${RUNNER} docker push ${REPO}/synapse-dask:${synapse_dask_version}
